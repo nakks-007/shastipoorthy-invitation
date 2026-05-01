b475bfe895b46f834c883642fabdd9ca063e68fe
naga <121928949+nakks-007@users.noreply.github.com>
2026-05-01 08:25:16 +0530

    Loader Implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,25 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from "./shared/footer/footer.component";
 import { NavbarComponent } from "./shared/navbar/navbar.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, NavbarComponent],
+  imports: [RouterOutlet, FooterComponent, NavbarComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'shasti-invite';
+  isLoading = true;
+
+  ngOnInit() {
+    window.addEventListener('load', () => {
+      setTimeout(() => {
+        this.isLoading = false;
+      }, 1200);
+    });
+  }
+
 }
