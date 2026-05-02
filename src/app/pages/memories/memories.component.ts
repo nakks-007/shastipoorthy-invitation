791d0bba10e1b0cc4a04b5afb0b9aa4a9ee897fa
naga <121928949+nakks-007@users.noreply.github.com>
2026-05-02 08:10:46 +0530

    Typos Fixed and Memories Screen Loader Implemented

--- src/app/pages/memories/memories.component.ts
+++ src/app/pages/memories/memories.component.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, QueryList, ViewChildren, ElementRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-memories',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './memories.component.html',
   styleUrl: './memories.component.css'
 })
-export class MemoriesComponent {
+export class MemoriesComponent implements AfterViewInit {
 
+  @ViewChildren('imgRef') images!: QueryList<ElementRef<HTMLImageElement>>;
+
+  totalImages = 0;
+  loadedImages = 0;
+  isLoading = true;
+
+  ngOnInit() {
+    this.resetLoader();
+
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 5000); // fallback
+  }
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.checkCachedImages();
+    }, 0);
+  }
+
+  resetLoader() {
+    this.totalImages = 11;
+    this.loadedImages = 0;
+    this.isLoading = true;
+  }
+
+  onImageLoad() {
+    this.loadedImages++;
+    this.checkAllLoaded();
+  }
+
+  checkCachedImages() {
+    this.images.forEach(img => {
+      if (img.nativeElement.complete) {
+        this.loadedImages++;
+      }
+    });
+
+    this.checkAllLoaded();
+  }
+
+  checkAllLoaded() {
+    if (this.loadedImages >= this.totalImages) {
+      this.isLoading = false;
+    }
+  }
 }
